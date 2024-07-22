cdf09e958cb716144bfa470a63c2c55c7e445c0e
maooam27 <52970070+maooam27@users.noreply.github.com>
2024-07-22 23:14:30 +0200

    fixed every button

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ class App(tk.Tk):
     def stroke_default(self):
         self.stroke = 1
 
+    def canvas_delete_all(self):
+        self.canvas.delete("all")
+
     # Pencil
     def pencil(self):
         self.penColor = "black"
@@ -96,21 +99,21 @@ class App(tk.Tk):
         self.label123 = tk.Label(self.holder, text="TOOLS", borderwidth=1, relief=tk.SOLID, width=15)
         self.label123.grid(row=0, column=0)
 
-        self.pencil_button = tk.Button(self.holder, text="Pencil", width=15)
+        self.pencil_button = tk.Button(self.holder, text="Pencil", width=15, command=self.pencil)
         self.pencil_button.grid(row=1, column=0)
-        self.eraser_button = tk.Button(self.holder, text="Eraser", width=15)
+        self.eraser_button = tk.Button(self.holder, text="Eraser", width=15, command=self.eraser)
         self.eraser_button.grid(row=2, column=0)
-        self.color_button = tk.Button(self.holder, text="Select Color", width=15)
+        self.color_button = tk.Button(self.holder, text="Select Color", width=15, command=self.color_choice)
         self.color_button.grid(row=3, column=0)
 
         self.other_label = tk.Label(self.holder, text="OTHER", borderwidth=1, relief=tk.SOLID, width=15)
         self.other_label.grid(row=0, column=1)
 
-        self.new_button = tk.Button(self.holder, text="New", width=15)
+        self.new_button = tk.Button(self.holder, text="New", width=15, command=self.new_file)
         self.new_button.grid(row=1, column=1)
-        self.clear_button = tk.Button(self.holder, text="Clear", width=15)
+        self.clear_button = tk.Button(self.holder, text="Clear", width=15, command=self.canvas_delete_all)
         self.clear_button.grid(row=2, column=1)
-        self.exit_button = tk.Button(self.holder, text="Exit", width=15)
+        self.exit_button = tk.Button(self.holder, text="Exit", width=15, command=self.destroy)
         self.exit_button.grid(row=3, column=1)
 
         self.stroke_label = tk.Label(self.holder, text="Stroke Size", borderwidth=1, relief=tk.SOLID, width=15)
@@ -120,7 +123,7 @@ class App(tk.Tk):
         self.increment_button.grid(row=1, column=2)
         self.decrement_button = tk.Button(self.holder, text="Decrement", width=15, command=self.stroke_decrement)
         self.decrement_button.grid(row=2, column=2)
-        self.default_button = tk.Button(self.holder, text="Default", width=15)
+        self.default_button = tk.Button(self.holder, text="Default", width=15, command=self.stroke_default)
         self.default_button.grid(row=3, column=2)
 
         # Create a canvas
